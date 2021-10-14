d6f7e92d76387c7f374308f7fd0348206e249067
jkl130 <842081147@qq.com>
2021-10-14 11:19:34 +0800

    111

--- src/main/java/com/anjsh/web/CommonController.java
+++ src/main/java/com/anjsh/web/CommonController.java
@@ -1,15 +1,20 @@
 package com.anjsh.web;
 
+import cn.hutool.core.img.ImgUtil;
 import com.anjsh.entity.Area;
 import com.anjsh.service.AreaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
-public class AreaController {
+public class CommonController {
 
     @Autowired
     private AreaService areaAervice;
@@ -32,6 +37,11 @@ public class AreaController {
     @GetMapping(value = "/findAreaByLevel3")
     public List<Area> findAreaByLevel3(String areaName, String cityName) {
         return areaAervice.findAreaByLevel(areaName, cityName);
+    }
+
+    @PostMapping("upload")
+    public String fileUpload(@RequestParam(value = "upload") MultipartFile file) throws IOException {
+        return ImgUtil.toBase64DataUri(ImgUtil.read(file.getInputStream()), "png");
     }
 
 }
