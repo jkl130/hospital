f174c0041b608e4ed8ad016fbfd1a8eb31ba9d3f
jkl130 <842081147@qq.com>
2021-10-29 15:54:19 +0800

    111

--- src/main/java/com/wly/App.java
+++ src/main/java/com/wly/App.java
@@ -3,12 +3,18 @@ package com.wly;
 import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.alibaba.fastjson.support.config.FastJsonConfig;
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
-import com.wly.utils.LoginHandlerInterceptor;
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.InnerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import com.qcloud.cos.COSClient;
+import com.qcloud.cos.ClientConfig;
+import com.qcloud.cos.auth.BasicCOSCredentials;
+import com.qcloud.cos.auth.COSCredentials;
+import com.qcloud.cos.region.Region;
+import com.wly.utils.LoginHandlerInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -31,6 +37,13 @@ import java.util.List;
 @MapperScan("com.wly.mapper")
 public class App implements WebMvcConfigurer {
 
+    @Value("${secretId}")
+    private String secretId;
+
+    @Value("${secretKey}")
+    private String secretKey;
+
+
     public static void main(String[] args) {
         // 启动一个springboot项目
         SpringApplication.run(App.class, args);
@@ -44,6 +57,18 @@ public class App implements WebMvcConfigurer {
                 .addPathPatterns("/**")
                 // 要排除拦截的path
                 .excludePathPatterns("/user/login");
+    }
+
+    @Bean(destroyMethod = "shutdown")
+    public COSClient cosClient() {
+        // 1 初始化用户身份信息(secretId, secretKey)
+        COSCredentials cred = new BasicCOSCredentials(secretId, secretKey);
+        // 2 设置 bucket 的地域, COS 地域的简称请参照 https://cloud.tencent.com/document/product/436/6224
+        Region region = new Region("ap-guangzhou");
+        // clientConfig 中包含了设置 region, https(默认 http), 超时, 代理等 set 方法, 使用可参见源码或者常见问题 Java SDK 部分
+        ClientConfig clientConfig = new ClientConfig(region);
+        // 3 生成 cos 客户端
+        return new COSClient(cred, clientConfig);
     }
 
     @Bean
--- src/main/java/com/wly/web/CommonController.java
+++ src/main/java/com/wly/web/CommonController.java
@@ -1,10 +1,14 @@
 package com.wly.web;
 
 import cn.hutool.core.collection.CollectionUtil;
-import cn.hutool.core.img.ImgUtil;
+import cn.hutool.core.util.IdUtil;
 import cn.hutool.core.util.StrUtil;
+import com.qcloud.cos.COSClient;
+import com.qcloud.cos.model.ObjectMetadata;
+import com.qcloud.cos.model.PutObjectRequest;
 import com.wly.entity.Area;
 import com.wly.service.AreaService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -27,6 +31,12 @@ public class CommonController {
     @Resource
     private AreaService areaService;
 
+    @Resource
+    private COSClient cosClient;
+
+    @Value("${bucketName}")
+    private String bucketName;
+
     @GetMapping(value = "/findAreaByLevel2")
     public List<Area> findAreaByLevel2(String areaName) {
         return areaService.findAreaByLevel2(areaName);
@@ -45,13 +55,20 @@ public class CommonController {
      */
     @PostMapping("upload")
     public String fileUpload(@RequestParam(value = "upload") MultipartFile file) throws IOException {
-        // 将文件转换成base64
-        return ImgUtil.toBase64DataUri(
-                // 读取图片
-                ImgUtil.read(file.getInputStream()),
-                // 取图片后缀作为图片的类型
-                CollectionUtil.getLast(StrUtil.split(file.getOriginalFilename(), "."))
-        );
+        ObjectMetadata objectMetadata = new ObjectMetadata();
+        String key = IdUtil.fastSimpleUUID() + "-" + file.getOriginalFilename();
+        // 取图片后缀作为图片的类型
+        objectMetadata.setContentType("image/" + CollectionUtil.getLast(StrUtil.split(file.getOriginalFilename(), ".")));
+        cosClient.putObject(new PutObjectRequest(
+                // 指定文件将要存放的存储桶
+                bucketName,
+                // 指定文件上传到 COS 上的路径，即对象键。例如对象键为folder/picture.jpg，则表示将文件 picture.jpg 上传到 folder 路径下
+                key,
+                file.getInputStream(),
+                objectMetadata
+        ));
+
+        return "https://" + bucketName + ".cos.ap-guangzhou.myqcloud.com/" + key;
     }
 
 }
